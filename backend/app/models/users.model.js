2d460ed9461820b69894468cf2c20edf28c34821
Zain Sadaqat <zain.sadaqet@gmail.com>
2023-07-31 23:20:59 +0500

    Create user model schema

---
+++ backend/app/models/users.model.js
@@ -0,0 +1,40 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// this will hash the password before saving it to the database
+userSchema.pre('save', async function (next) {
+  if (!this.isModified()) {
+    next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
