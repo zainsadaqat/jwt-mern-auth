3264dc0ffc7d51818d6d9c60c92546c7a9e0601f
Zain Sadaqat <zain.sadaqet@gmail.com>
2023-08-02 23:27:51 +0500

    Dispatch login functionality

--- frontend/src/screens/LoginScreen.jsx
+++ frontend/src/screens/LoginScreen.jsx
@@ -1,15 +1,37 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import { Form, Button, Row, Col } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
 import FormContainer from '../components/FormContainer';
+import { useLoginMutation } from '../redux/slices/usersApiSlice';
+import { setCredentials } from '../redux/slices/authSlice';
 
 const LoginScreen = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const [login, { isLoading }] = useLoginMutation();
+
+  const { userInfo } = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    if (userInfo) {
+      navigate('/');
+    }
+  }, [navigate, userInfo]);
+
   const submitHandler = async (e) => {
     e.preventDefault();
-    console.log('Form Submitted!');
+    try {
+      const res = await login({ email, password }).unwrap();
+      dispatch(setCredentials(...res));
+      navigate('/');
+    } catch (err) {
+      console.log(err?.data?.message || err.error);
+    }
   };
   return (
     <FormContainer>
@@ -32,7 +54,7 @@ const LoginScreen = () => {
           <Form.Control
             type="password"
             placeholder="Enter password..."
-            value={email}
+            value={password}
             onChange={(e) => setPassword(e.target.value)}
           ></Form.Control>
         </Form.Group>
@@ -40,7 +62,7 @@ const LoginScreen = () => {
         <Button type="submit" variant="primary" className="mt-3">
           Sign in
         </Button>
-        
+
         <Row className="py-3">
           <Col>
             New Customer? <Link to="/register">Register</Link>
